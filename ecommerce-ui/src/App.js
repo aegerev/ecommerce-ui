cd0d94427cb8d920d33e3b1d96c9900a9129e4f9
alex <alexandersegerev@gmail.com>
2024-08-27 15:55:04 -0400

    Listed the products in the e-commerce application

--- ecommerce-ui/src/App.js
+++ ecommerce-ui/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import {Navbar, Container, Nav, Badge} from "react-bootstrap"
+import {Navbar, Container, Nav, Badge, Card, Button} from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
   const products = [
     { id: 1, name: "My Best Friend is Ariel", price: 8.16},
@@ -16,6 +20,8 @@ function App() {
     { id: 8, name: "Tonies Ariel Audio Play Character from Disney's The Little Mermaid", price: 17.99 },
     { id: 9, name: "Punkt. MP02 4G LTE Minimalist Mobile Phone", price: 229.00 },
     { id: 10, name: "reMarkable Bundle 2 Notetaking Tablet", price: 429.00 },
+    { id: 11, name: "ecobee3 Lite Smart Thermostat", price: 129.00 },
+    { id: 12, name: "Apple iPad (10th Generation)", price: 122.00 },
 ];
 
   return (
@@ -30,6 +36,28 @@ function App() {
           </Nav>
          </Container>
       </Navbar>
+
+      <Container>
+        <h2 className="mt-4 mb-4">Products</h2>
+        <div className = "row">
+        {products.map((product) => (
+                        <div key={product.id} className="col-md-4 mb-4">
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>{product.name}</Card.Title>
+                                    <Card.Text>${product.price}</Card.Text>
+                                    <Button
+                                        onClick={() => addToCart(product)}
+                                        variant="primary"
+                                    >
+                                        Add to Cart
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    ))}
+        </div>
+      </Container>
     </div>
   );
 
